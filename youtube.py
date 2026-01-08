04c30823a0efb22c94dd3db51679f11e77f23693
Levi-o1o1 <ayux.exe@gmail.com>
2026-01-08 19:24:31 +0530

    YouTube_1

--- youtube.py
+++ youtube.py
@@ -3,16 +3,12 @@ from yt_dlp import YoutubeDL
 url = ""
 
 
-
-
-
-
-
-
-
-
-
-# ignore this:
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ydl_opts =  { 'format': 'bestvideo+bestaudio/best','merge_output_format': 'mp4',}
 ydl_opts ={ 'outtmpl': '%(title)s.%(ext)s',}
 with YoutubeDL(ydl_opts) as ydl:ydl.download([url])
+
+# Note: you need to install yt_dlp library first
+# HOW TO INSTALL:
+# pip install yt_dlp 
+# if show any erorr like module not found so you need .venv python virtual inverment 
